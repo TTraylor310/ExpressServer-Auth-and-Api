40621532893c81e38d6c5c86935244bb9f4f0850
Tim Traylor <ttraylor83@gmail.com>
2022-10-02 14:21:59 -0700

    added seeding.js to enable manual seed of database for display

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,8 @@ const musicModel = require('./music.js');
 const userModel = require('../auth/models/users.js');
 const modelInterface = require('./model-interface.js');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory:';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+// const DATABASE_URL = 'sqlite:memory:';
 
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
@@ -21,14 +22,18 @@ const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   typeValidation: true,
 };
 
-// userModel.hasMany(moviesModel);
-// userModel.hasMany(musicModel);
-
 
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 const movies = moviesModel(sequelize, DataTypes);
 const music = musicModel(sequelize, DataTypes);
 const user = userModel(sequelize, DataTypes);
+
+// music.belongsTo(movies);
+
+// music.hasMany(movies, {foreignKey: 'year'});
+// movies.belongsTo(music, {foreignKey: 'year'});
+// movies.find({where: {...}, include: [music]});
+// movies.findAll({where: {...}, include: [music]});
 
 module.exports = {
   db: sequelize,
